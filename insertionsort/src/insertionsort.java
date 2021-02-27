22f17e0ec4e401dd0d5cb728625a73230d81952b
Sofia Esparza-Chavez <sesparzachavez2019@aolp.org>
2021-02-26 16:02:44 -0800

    added algos

---
+++ insertionsort/src/insertionsort.java
@@ -0,0 +1,5 @@
+public class insertionsort {
+    public static void main(String args[]){
+        System.out.println("hello insertion sort");
+    }
+}
---
+++ selectionsort/src/selectionsort.java
@@ -0,0 +1,5 @@
+public class selectionsort {
+    public static void main(String args[]){
+        System.out.println("hello selection sort");
+    }
+}
